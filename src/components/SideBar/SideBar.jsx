1145003d23b74ff22f26993c571ce08e7de2b95f
mateussilvasouza <mathheussilvasouza@gmail.com>
2022-10-05 15:06:40 -0300

    SideBar Complete

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,18 +1,31 @@
 import styles from './SideBar.module.css'
-import Perfil from './../../assets/perfil1.svg'
+import { PencilLine } from 'phosphor-react'
+
+import Capa from './../../assets/bgAside.svg'
+import Perfil from './../../assets/perfil.jpeg'
 
 export function SideBar(){
     return(
-        <aside className={styles.aside}>
-            <header>
-                <img className={styles.perfil} src={Perfil} alt="" />
-            </header>
-            <main>
-                <h3>Leslie Alexander</h3>
+        <aside className={styles.sidebar}>
+            <img
+                className={styles.cover}
+                src={Capa}
+            />
+
+            <div className={styles.profile}>
+                <img
+                    className={styles.avatar}
+                    src={Perfil}
+                />
+                <strong>Leslie Alexander</strong>
                 <span>UI Designer</span>
-            </main>
+            </div>
+
             <footer>
-                <a href="#"> Editar seu perfil</a>
+                <a href="#">
+                    <PencilLine size={20}/>
+                     Editar seu perfil
+                </a>
             </footer>
 
         </aside>
